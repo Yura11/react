330f461080164e2a96b7c4de3dadc800f49fc619
Yura11 <y.belichenko007@gmail.com>
2024-03-22 22:05:53 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import About from './components/About/About';
+import ProductList from './components/ProductList/ProductList';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "bulma/css/bulma.css";
+import Contact from './components/Contact/Contact';
+import AddProduct from './components/AddProduct/AddProduct';
+import EditProduct from './components/EditProduct/EditProduct';
+import Header from './components/Header/Header';
 
 function App() {
+  const [title, setTitle] = useState("Hello, students!");
+
+  
+  const [products, setProducts] = useState([]); 
+  
+
+  const clickMe = () => {
+    setTitle('Title is changed!');
+    console.log("It's clicked!");
+  };
+
+  const deleteProduct = (productId) => {
+    const newProducts = products.filter(product => product.id !== productId );
+    setProducts(newProducts);
+  };
+
+  const [name, setName] = useState('Yura');
+
+  useEffect(() => {
+    console.log('useEffect');
+  }, [name]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<ProductList products={ products } deleteProduct={ deleteProduct } />} />
+          <Route path='/about' element={<About/>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
+          <Route path='/add' element={<AddProduct/>}></Route>
+          <Route path='/edit/:id' element={<EditProduct/>}></Route>
+        </Routes>
+      </BrowserRouter>
+  
+      <div>
+        <button className="button is-primary mt-5" onClick={() => { setName('John') }}>Change name</button>
+        <p>Name: {name}</p>
+      </div>
     </div>
   );
 }
